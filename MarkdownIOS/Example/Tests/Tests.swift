0a15ec8ca3b1e9a9e0cd57d09b65a347fb5ac0e2
Johan Bos <johan@Johans-MacBook.local>
2016-09-06 22:07:09 +0200

    Added test framework for iOS with the first unit test

--- MarkdownIOS/Example/Tests/Tests.swift
+++ MarkdownIOS/Example/Tests/Tests.swift
@@ -1,29 +1,103 @@
 import UIKit
 import XCTest
-import SimpleMarkdownParser
+@testable import SimpleMarkdownParser
 
+// Test class
 class Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // --
+    // MARK: Test cases
+    // --
+
+    // Test case with given text and expected markdown tags
+    func testFindTagsNewLines() {
+        let markdownTextLines = [
+            "",
+            "",
+            "Text",
+            "",
+            "",
+            "",
+            "Another",
+            ""
+        ]
+        let expectedTags = [
+            WrappedMarkdownTag(type: .Normal, flags: MarkdownTag.FLAG_NONE, text: "Text"),
+            WrappedMarkdownTag(type: .Paragraph, weight: 1, flags: MarkdownTag.FLAG_NONE, text: ""),
+            WrappedMarkdownTag(type: .Normal, flags: MarkdownTag.FLAG_NONE, text: "Another")
+        ]
+        assertTags(markdownTextLines, expectedTags: expectedTags)
     }
+
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    // --
+    // MARK: Helpers
+    // --
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func assertTags(markdownTextLines: [String], expectedTags: [WrappedMarkdownTag], file: String = #file, line: UInt = #line) {
+        let parser = SimpleMarkdownParserSwift()
+        let markdownText = markdownTextLines.joinWithSeparator("\n")
+        let foundTags = parser.findTags(markdownText)
+        for i in 0..<min(foundTags.count, expectedTags.count) {
+            let wrappedTag = WrappedMarkdownTag(markdownText: markdownText, tag: foundTags[i])
+            if expectedTags[i] != wrappedTag {
+                recordFailureWithDescription("Tags not equal, expected: \(expectedTags[i]), but having: \(wrappedTag)", inFile: file, atLine: line, expected: true)
+            }
         }
+        if expectedTags.count != foundTags.count {
+            recordFailureWithDescription("Missing or too many tags, expected: \(expectedTags.count), but having: \(foundTags.count)", inFile: file, atLine: line, expected: true)
+        }
+        XCTAssertEqual(expectedTags.count, foundTags.count)
+    }
+
+}
+
+// Helper class to simply compare tags
+class WrappedMarkdownTag: NSObject {
+    
+    private var type: MarkdownTagType
+    private var flags: Int
+    private var weight: Int
+    private var text: String
+    private var extra: String
+    
+    override var description : String {
+        return "{ type: \(type), flags: \(flags), weight: \(weight), text: \(text), extra: \(extra)"
     }
     
+    convenience init(type: MarkdownTagType, flags: Int, text: String) {
+        self.init(type: type, weight: 0, flags: flags, text: text, extra: "")
+    }
+
+    convenience init(type: MarkdownTagType, flags: Int, text: String, extra: String) {
+        self.init(type: type, weight: 0, flags: flags, text: text, extra: extra)
+    }
+
+    convenience init(type: MarkdownTagType, weight: Int, flags: Int, text: String) {
+        self.init(type: type, weight: weight, flags: flags, text: text, extra: "")
+    }
+
+    init(type: MarkdownTagType, weight: Int, flags: Int, text: String, extra: String) {
+        self.type = type
+        self.weight = weight
+        self.flags = flags
+        self.text = text
+        self.extra = extra
+    }
+    
+    convenience init(markdownText: String, tag: MarkdownTag) {
+        self.init(
+            type: tag.type,
+            weight: tag.weight,
+            flags: tag.flags,
+            text: SimpleMarkdownParserSwift().extractText(markdownText, tag: tag),
+            extra: SimpleMarkdownParserSwift().extractExtra(markdownText, tag: tag)
+        )
+    }
+    
+    override func isEqual(object: AnyObject?) -> Bool {
+        guard let other = object as? WrappedMarkdownTag else { return false }
+        return self.type == other.type && self.flags == other.flags && self.weight == other.weight && self.text == other.text && self.extra == other.extra
+    }
+
 }
